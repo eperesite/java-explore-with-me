a637f694a3e34f74c3f56948a9214f51d6426853
User Podsekina <margaritapodsekina@gmail.com>
2024-12-24 00:03:58 +0300

    Добавила ветку

--- main-service/src/main/java/ru/practicum/ewm/request/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/request/RequestRepository.java
@@ -7,11 +7,18 @@ import java.util.Optional;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
     List<Request> findAllByEventId(Long eventId);
+
     List<Request> findAllByRequestIdIn(List<Long> requestIds);
+
     List<Request> findAllByEventIdInAndStatus(List<Long> eventIds, RequestStatus status);
+
     Optional<Request> findByRequestIdAndRequesterId(Long requestId, Long requesterId);
+
     List<Request> findAllByRequesterId(Long userId);
+
     Optional<List<Request>> findByEventIdAndRequestIdIn(Long eventId, List<Long> requestIds);
+
     int countByEventIdAndStatus(Long eventId, RequestStatus status);
+
     Boolean existsByEventIdAndRequesterId(Long eventId, Long userId);
 }
